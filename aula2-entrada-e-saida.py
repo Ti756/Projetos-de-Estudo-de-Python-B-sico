0667e4aba7cc52598bca992dc33ccc059da366ff
Ti_Da_Zuera <54287909+Ti756@users.noreply.github.com>
2020-06-13 14:12:23 -0300

    Add files via upload

---
+++ aula2-entrada-e-saida.py
@@ -0,0 +1,9 @@
+numero1  =  input ('Escreva um numero:  ')
+numero2  =  input ('Escreva outro numero:  ')
+
+resultado  =  numero1 + numero2
+
+print (resultado)
+
+
+
---
+++ aula3-operadores-logicos.py
@@ -0,0 +1,5 @@
+var_verdade  =   True_# Boolean
+var_falso  =   False
+
+if var_verdade  ==   True:
+    print('Var_verdade é:',  var_verdade)
